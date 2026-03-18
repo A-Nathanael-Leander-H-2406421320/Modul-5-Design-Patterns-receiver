016d3fc27c8d4f5e952f56804163a04916f479e6
nathanaeru <nathanael.leander@ui.ac.id>
2026-03-18 11:46:52 +0700

    Implement receive_notification function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -76,4 +76,9 @@ impl NotificationService {
             Self::unsubscribe_request(product_type_clone))
             .join().unwrap();
     }
+
+    pub fn receive_notification(payload: Notification) -> Result<Notification> {
+        let subscriber_result: Notification = NotificationRepository::add(payload);
+        return Ok(subscriber_result);
+    }
 }
